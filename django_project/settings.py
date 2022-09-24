9b38d152a360e5f4127cabb07713bb5b16fa6078
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2022-09-24 12:37:30 +0000

    change settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -62,18 +63,24 @@ DEBUG = 'DEV' in os.environ
 # DEBUG = True
 
 ALLOWED_HOSTS = [
-    # 'recipe-star-3.herokuapp.com', not working 
+    # os.environ.get('ALLOWED_HOST'), # not working 
     'recipe-star-3-api.herokuapp.com',
     'localhost',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN')
+#     ]
+# else:
+#     CORS_ALLOWED_ORIGIN_REGEXES = [
+#         r"^https://.*\.gitpod\.io$",
+#     ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
