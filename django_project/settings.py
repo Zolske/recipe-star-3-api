4d11b154af546be5ee322c5bf072394ec08ef7ca
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2022-09-24 11:12:50 +0000

    fix typo in settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,7 +47,7 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'django_project.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
